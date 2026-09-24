46890e98701f45a9810438ba84161d8456ebcc33
Jiordi Kengne <jiordikengne@gmail.com>
2026-09-24 11:33:22 +0200

    feat(site): add Vercel Analytics (#40)

--- site/src/main.tsx
+++ site/src/main.tsx
@@ -1,3 +1,4 @@
+import { Analytics } from "@vercel/analytics/react"
 import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
 import { BrowserRouter } from "react-router-dom"
@@ -11,6 +12,7 @@ createRoot(document.getElementById("root")!).render(
     <ThemeProvider>
       <BrowserRouter>
         <App />
+        <Analytics />
       </BrowserRouter>
     </ThemeProvider>
   </StrictMode>

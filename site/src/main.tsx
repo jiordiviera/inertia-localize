7eb92d04a42b791a8c507cfed8c4b2e38b91a4e3
Jiordi Kengne <jiordikengne@gmail.com>
2026-09-24 06:20:40 +0200

    chore(site): add Vercel Web Analytics
    Enable pageview tracking on the docs site now that it's deployed at
    inertia-localize.jiordiviera.me.

--- site/src/main.tsx
+++ site/src/main.tsx
@@ -1,3 +1,4 @@
+import { Analytics } from '@vercel/analytics/react'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
@@ -9,5 +10,6 @@ createRoot(document.getElementById('root') as HTMLElement).render(
     <BrowserRouter>
       <App />
     </BrowserRouter>
+    <Analytics />
   </StrictMode>,
 )

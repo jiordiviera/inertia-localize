56bd8849c98a219060021e427c0fffed4bde4f0b
Jiordi Kengne <jiordikengne@gmail.com>
2026-09-25 10:59:14 +0200

    feat(site): add vercel speed insights
    * config: add @vercel/speed-insights to site/package.json; update pnpm-lock.yaml* other: integrate <SpeedInsights /> component in site/src/main.tsx

--- site/src/main.tsx
+++ site/src/main.tsx
@@ -1,4 +1,5 @@
 import { Analytics } from "@vercel/analytics/react"
+import {SpeedInsights} from "@vercel/speed-insights/react"
 import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
 import { BrowserRouter } from "react-router-dom"
@@ -13,6 +14,7 @@ createRoot(document.getElementById("root")!).render(
       <BrowserRouter>
         <App />
         <Analytics />
+        <SpeedInsights />
       </BrowserRouter>
     </ThemeProvider>
   </StrictMode>
